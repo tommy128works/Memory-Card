fe0b46c2b2e142fd73d4a6ee971f5301c3100fd0
Tommy <tommy128works@gmail.com>
2023-08-29 11:01:14 -0400

    feat: add Header
    No functionality added. Future functionality includes:
    - reset game to display main menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
-    <div>
-      <p>default paragraph</p>
-      <p>default paragraph</p>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "../styles/Header.css";
+import pokeball from "../assets/pokeball_icon.png";
+
+function Header() {
+
+  return (
+    <div id="header" >
+      <img src={pokeball} alt="pokeball icon" />
+
+      <div>
+        <span className="red-text" >Pokémon </span>
+        <span className="black-text" >Memory</span>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Header;
