f615284bbc8efa5047ac770db6415d66e20ac101
Tommy <tommy128works@gmail.com>
2023-08-29 11:26:55 -0400

    fix: Footer stays at bottom of page
    Fixed positioning error of Footer. Previously, flexbox rules were
    applied to :root instead of #root.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import MainMenu from "./components/MainMenu.jsx";
 
 function App() {
   return (
     <>
       <div id="content-container">
         <Header />
+        <MainMenu />
       </div>
 
       <Footer />
---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,11 @@
+import "../styles/MainMenu.css";
+
+function MainMenu() {
+  return (
+    <div>
+      bbbbbbbbbbbbbbbbbbbbbbbig paragraph
+    </div>
+  )
+}
+
+export default MainMenu;
