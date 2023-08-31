c8114a3dd78744d87bd8feda19efc455a6e50505
Tommy <tommy128works@gmail.com>
2023-08-31 12:21:30 -0400

    feat: add MainMenu
    No functionality such as event listeners added.

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -2,10 +2,20 @@ import "../styles/MainMenu.css";
 
 function MainMenu() {
   return (
-    <div>
-      bbbbbbbbbbbbbbbbbbbbbbbig paragraph
+    <div id="main-menu">
+      <div id="main-menu-modal">
+        <div id="main-menu-modal-content">
+          <div id="main-menu-title" >Gain points by clicking each card only once!</div>
+
+          <div id="main-menu-buttons-container" >
+            <button id="main-menu-easy-button" >Easy</button>
+            <button id="main-menu-medium-button" >Medium</button>
+            <button id="main-menu-hard-button" >Hard</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default MainMenu;
